cc16701d10a270638f00e14be953110730190f7a
Your-Arun <arun.work82@gmail.com>
2026-01-02 13:54:30 +0530

    hgg

---
+++ Fro/App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler'; 
+import { registerRootComponent } from 'expo';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+import Toast from 'react-native-toast-message';
+import { UserProvider } from './src/services/UserContext';
+
+function App() {
+  return (
+    <> <UserProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+      <Toast />
+      </UserProvider>
+    </>
+  );
+}
+
+registerRootComponent(App);
